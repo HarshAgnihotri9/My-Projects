3eeb8616d5ab95d4f639c342bcb5c0591a584988
Harsh Agnihotri <140311544+HarshAgnihotri9@users.noreply.github.com>
2023-12-21 18:47:00 -0800

    Add files via upload

---
+++ jeweller website/1.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+// var ul=document.querySelector('.ul');
+//   myfunction(e); {
+//    console.log(e);
+//    if(ul.style.display === "none"){
+//      display = "block"
+//    }
+//    else{
+//       display ="none"
+//    }
+// }
